c58b2b571fabf14b9e7405c371d794b467152753
4m4r199707 <4m4rarts@gmail.com>
2021-11-19 19:06:22 +0530

    Intitial Setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAuA99Vzh6ByriVbLChURrDOZXcLEF2WR0",
+    authDomain: "watsapp-mern-8cfa0.firebaseapp.com",
+    projectId: "watsapp-mern-8cfa0",
+    storageBucket: "watsapp-mern-8cfa0.appspot.com",
+    messagingSenderId: "755462844683",
+    appId: "1:755462844683:web:66d8093bb1a693f622b20e"
+  };
